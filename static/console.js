7e7134346275339df15317e080263e522852f8a7
Nilay Kumar Shrivastava <nilaykshrivastava@gmail.com>
2026-07-07 09:13:30 +0530

    Update console.js

--- static/console.js
+++ static/console.js
@@ -21,6 +21,7 @@ if (!window.__POPCHATS_CONSOLE__) {
 
     console.warn(
       `This panel is meant for developers and may look overwhelming to visitors. So visitors, you don't have anything to do here!`
+    );
 
     console.warn(
       `Do not copy-paste random code snippets which you don't understand.`
