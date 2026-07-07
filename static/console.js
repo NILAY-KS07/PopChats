73735220acfae58b580a07d70405747468865a8b
Nilay Kumar Shrivastava <nilaykshrivastava@gmail.com>
2026-07-07 09:08:34 +0530

    Add developer console log messages

---
+++ static/console.js
@@ -0,0 +1,28 @@
+if (!window.__POPCHATS_CONSOLE__) {
+    window.__POPCHATS_CONSOLE__ = true;
+
+    console.clear();
+
+    console.log(
+      
+`██████╗  ██████╗ ██████╗  ██████╗██╗  ██╗ █████╗ ████████╗███████╗
+██╔══██╗██╔═══██╗██╔══██╗██╔════╝██║  ██║██╔══██╗╚══██╔══╝██╔════╝
+██████╔╝██║   ██║██████╔╝██║     ███████║███████║   ██║   ███████╗
+██╔═══╝ ██║   ██║██╔═══╝ ██║     ██╔══██║██╔══██║   ██║   ╚════██║
+██║     ╚██████╔╝██║     ╚██████╗██║  ██║██║  ██║   ██║   ███████║
+╚═╝      ╚═════╝ ╚═╝      ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ╚══════╝`
+      
+    );
+
+    console.log(
+      "%PopChats Developer Console",
+      "color:#4f8cff;font-size:18px;font-weight:bold;"
+    );
+
+    console.warn(
+      `This panel is meant for developers and may look overwhelming to visitors. So visitors, you don't have anything to do here!`
+
+    console.warn(
+      `Do not copy-paste random code snippets which you don't understand.`
+    );
+}
